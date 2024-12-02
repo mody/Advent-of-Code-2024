a187e698b8f621b8c99d06f58ff87cf882275b71
Patrik Modesto <patrik.modesto@gmail.com>
2024-12-02 21:02:40 +0100

    day 2 - part 2

--- src/day2.cc
+++ src/day2.cc
@@ -2,47 +2,91 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
-#include <range/v3/view/drop.hpp>
-
 #include <boost/algorithm/string/constants.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
 
 #include <iostream>
 #include <vector>
+#include <ranges>
 
 using Level = std::vector<unsigned>;
 using Levels = std::vector<Level>;
+
+int find_error(Level const& level)
+{
+    unsigned err{0};
+    if (level.at(0) == level.at(1))
+    {
+        return err;
+    }
+    const auto sign {level.at(0) > level.at(1) ? 1 : -1};
+
+    unsigned last {level.at(0)};
+    for (auto val : level | std::views::drop(1))
+    {
+        ++err;
+        const auto diff {(last - val) * sign};
+        last = val;
+        if (diff >= 1 && diff <= 3)
+        {
+            // ok
+        }
+        else
+        {
+            return err;
+        }
+    }
+    return -1;
+}
+
 
 void part1(Levels levels)
 {
     unsigned safe{0};
 
     for (auto const& level : levels) {
-        if (level.at(0) == level.at(1))
-        {
-            continue;
-        }
-        const auto sign {level.at(0) > level.at(1) ? 1 : -1};
-
-        bool valid{true};
-        unsigned last {level.at(0)};
-        for (auto val : level | ranges::views::drop(1))
-        {
-            const auto diff {(last - val) * sign};
-            last = val;
-            if (diff >= 1 && diff <= 3) {
-                // ok
-            } else {
-                valid = false;
-                break;
-            }
-        }
-        if (valid)
+        if (find_error(level) == -1)
             ++safe;
     }
 
     fmt::println("1: {}", safe);
+}
+
+
+void part2(Levels levels)
+{
+    unsigned safe{0};
+
+    auto test_level = [](Level level) -> bool
+    {
+        auto idx = find_error(level);
+        if (idx == -1)
+        {
+            return true;
+        }
+        level.erase(level.begin() + idx);
+        idx = find_error(level);
+        return (idx == -1);
+    };
+
+    for (auto level : levels)
+    {
+        if (test_level(level))
+        {
+            ++safe;
+        }
+        else
+        {
+            std::reverse(level.begin(), level.end());
+            if (test_level(level))
+            {
+                ++safe;
+            }
+        }
+    }
+
+    fmt::println("2: {}", safe);
 }
 
 
@@ -69,6 +113,7 @@ int main()
     }
 
     part1(levels);
+    part2(levels);
 
     return 0;
 }
