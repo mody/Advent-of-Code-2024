9be2858e43b2a09ba0d9c79798dd3f1ad47318a9
Patrik Modesto <patrik.modesto@gmail.com>
2024-12-08 20:50:21 +0100

    day 5 - part 2

--- src/day5.cc
+++ src/day5.cc
@@ -1,12 +1,13 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
-#include <boost/algorithm/string/constants.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/constants.hpp>
 #include <boost/algorithm/string/split.hpp>
 
 #include <boost/container_hash/hash.hpp>
 
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
 #include <ranges>
@@ -17,34 +18,42 @@ using Ordering = std::unordered_set<std::pair<unsigned, unsigned>, boost::hash<s
 using Update = std::vector<unsigned>;
 using Updates = std::vector<Update>;
 
-void part1(Ordering const& ordering, Updates const& updates)
+void process(Ordering const& ordering, Updates const& updates)
 {
-    uint64_t sum{0};
+    uint64_t sum1 {0}, sum2 {0};
 
-    for (auto const& update : updates) {
-        fmt::println("{}", std::views::all(update));
+    for (auto update : updates)
+    {
         bool ok {true};
         unsigned prev = update.at(0);
-        for (auto const& val : update | std::views::drop(1)) {
-            if (ordering.contains(std::make_pair(prev, val))) {
+        for (auto const& val : update | std::views::drop(1))
+        {
+            if (ordering.contains(std::make_pair(prev, val)))
+            {
                 // pass
-            } else if (ordering.contains(std::make_pair(val, prev))) {
+            }
+            else if (ordering.contains(std::make_pair(val, prev)))
+            {
                 // fail
                 ok = false;
                 break;
-            } else {
-                fmt::println("\t{}|{} NOT FOUND", prev, val);
             }
             prev = val;
         }
-        // fmt::println("{}", ok ? "\tOK" : "\tFAILED");
-        if (ok) {
-            sum += update.at(update.size() / 2);
+        if (ok)
+        {
+            sum1 += update.at(update.size() / 2);
+        }
+        else
+        {
+            std::ranges::sort(
+                update, [&ordering](unsigned a, unsigned b) { return ordering.contains(std::make_pair(a, b)); });
+            sum2 += update.at(update.size() / 2);
         }
     }
-    fmt::println("1: {}", sum);
+    fmt::println("1: {}", sum1);
+    fmt::println("2: {}", sum2);
 }
-
 
 int main()
 {
@@ -52,9 +61,10 @@ int main()
     Updates updates;
 
     std::string line;
-    while(std::getline(std::cin, line))
+    while (std::getline(std::cin, line))
     {
-        if (line.empty()) break;
+        if (line.empty())
+            break;
 
         std::vector<std::string> parts;
         boost::algorithm::split(parts, line, boost::algorithm::is_any_of("|"), boost::token_compress_on);
@@ -62,22 +72,23 @@ int main()
         ord.insert(std::make_pair(std::stoi(parts.at(0)), std::stoi(parts.at(1))));
     }
 
-    while(std::getline(std::cin, line))
+    while (std::getline(std::cin, line))
     {
-        if (line.empty()) break;
+        if (line.empty())
+            break;
 
         std::vector<std::string> parts;
         boost::algorithm::split(parts, line, boost::algorithm::is_any_of(","), boost::token_compress_on);
 
         Update up;
-        for (auto const& v : parts) {
+        for (auto const& v : parts)
+        {
             up.push_back(std::stoul(v));
         }
         updates.push_back(std::move(up));
     }
 
-    part1(ord, updates);
-
+    process(ord, updates);
 
     return 0;
 }
