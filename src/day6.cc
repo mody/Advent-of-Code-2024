d94b61782365c24a0b47ce0526a8b5606b5421f3
Patrik Modesto <patrik.modesto@gmail.com>
2024-12-10 21:06:16 +0100

    day 6 - part 2 - WIP

--- src/day6.cc
+++ src/day6.cc
@@ -3,10 +3,6 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
-#include <boost/algorithm/string/constants.hpp>
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/split.hpp>
-
 #include <iostream>
 #include <map>
 #include <unordered_map>
@@ -16,6 +12,7 @@ using Coord = int32_t;
 using Point = Gfx_2d::Point<Coord>;
 
 using Map = std::unordered_map<Point, char, boost::hash<Point>>;
+using Visited = std::unordered_map<Point, std::unordered_set<Gfx_2d::Direction, boost::hash<Gfx_2d::Direction>>, boost::hash<Point>>;
 
 struct Guard
 {
@@ -30,25 +27,89 @@ static const std::map<char, Gfx_2d::Direction> TO_DIR{
     {'>', Gfx_2d::Right},
 };
 
-void part1(Map const& world, Guard g)
+std::pair<Visited, bool> walk_guard(Map const& world, Guard g)
 {
-    std::unordered_set<Point, boost::hash<Point>> visited;
-    visited.insert(g.px);
+    Visited visited;
+    bool finished{true};
 
     for (; world.contains(g.px);)
     {
-        Point next {g.px + g.dir};
-        const auto it = world.find(next);
-        if (it == world.end()) break;
-        if (it->second == '#') {
-            next = g.px;
+        auto [vit, inserted] = visited.insert({g.px, {g.dir}});
+        if (!inserted)
+        {
+            if (!vit->second.insert(g.dir).second)
+            {
+                finished = false;
+                break;
+            }
+        }
+
+        const Point next {g.px + g.dir};
+        const auto it {world.find(next)};
+        if (it == world.end())
+        {
+            break;
+        }
+        if (it->second == '#')
+        {
             g.dir = g.dir.cw90();
         }
-        g.px = next;
-        visited.insert(g.px);
+        else
+        {
+            g.px = next;
+        }
     }
 
+    return std::make_pair(visited, finished);
+}
+
+void part1(Map const& world, Guard g)
+{
+    auto const& [visited, finished] = walk_guard(world, g);
     fmt::println("1: {}", visited.size());
+}
+
+void part2(Map world, Guard const& g)
+{
+    const Visited visited {walk_guard(world, g).first};
+
+    std::unordered_set<Point, boost::hash<Point>> walls;
+
+    for (auto const& [px, dirs] : visited)
+    {
+        for (auto const& dir : dirs)
+        {
+            if (dirs.contains(dir.back())) continue; // tam i zpet, nemuzu zastavet
+
+            const auto pnext {px + dir};
+            if (auto wit {world.find(pnext)}; wit != world.end() && wit->second == '.')
+            {
+                const auto side_dir {dir.cw90()};
+                bool wall {false};
+                for (auto side_px {px + side_dir}; world.contains(side_px); side_px += side_dir)
+                {
+                    if (world.at(side_px) == '#')
+                    {
+                        wall = true;
+                        break;
+                    }
+                }
+                if (wall)
+                {
+                    wit->second = '#';
+                    if (!walk_guard(world, g).second)
+                    {
+                        walls.insert(pnext);
+                    }
+                    wit->second = '.';
+                }
+            }
+        }
+    }
+
+    walls.erase(g.px);
+
+    fmt::println("2: {}", walls.size());
 }
 
 int main()
@@ -82,6 +143,7 @@ int main()
     }
 
     part1(world, guard);
+    part2(world, guard);
 
     return 0;
 }
