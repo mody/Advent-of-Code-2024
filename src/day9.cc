e0e3c85157fd2f0bc6365465aa223371712e775f
Patrik Modesto <patrik.modesto@gmail.com>
2024-12-12 22:10:43 +0100

    day 9 - part 2

--- src/day9.cc
+++ src/day9.cc
@@ -8,20 +8,27 @@
 #include <vector>
 
 using Id = int;
-using Disk = std::vector<Id>;
+using Disk1 = std::vector<Id>;
 
-void part1(Disk disk)
+struct Item
 {
-    Disk::iterator from {disk.begin()};
-    Disk::iterator to {std::prev(disk.end())};
+    Id id {-1};
+    unsigned len {0};
+};
+using Disk2 = std::vector<Item>;
+
+void part1(Disk1 disk1)
+{
+    Disk1::iterator from {disk1.begin()};
+    Disk1::iterator to {std::prev(disk1.end())};
 
     for (;;)
     {
-        while (from != disk.end() && *from != -1)
+        while (from != disk1.end() && *from != -1)
             ++from;
-        while (to != disk.begin() && *to == -1)
+        while (to != disk1.begin() && *to == -1)
             --to;
-        if (from == disk.end() || to == disk.begin() || std::distance(from, to) <= 0)
+        if (from == disk1.end() || to == disk1.begin() || std::distance(from, to) <= 0)
             break;
         std::swap(*from, *to);
         ++from;
@@ -29,7 +36,7 @@ void part1(Disk disk)
     }
 
     uint64_t crc {0};
-    for (auto [idx, id] : ranges::views::enumerate(disk))
+    for (auto [idx, id] : ranges::views::enumerate(disk1))
     {
         if (id > 0)
         {
@@ -40,9 +47,70 @@ void part1(Disk disk)
     fmt::println("1: {}", crc);
 }
 
+void part2(Disk2 disk2)
+{
+    Disk2::iterator from {std::prev(disk2.end())};
+
+    Id actual_id{from->id};
+    assert(actual_id != -1);
+
+    for (; from != disk2.begin(); --from)
+    {
+        if (from->id != actual_id)
+            continue;
+
+        for (auto to {disk2.begin()}; to != disk2.end(); ++to)
+        {
+            if (to->id == actual_id)
+                break;  // too far
+
+            if (to->id == -1 && to->id != from->id)
+            {
+                if (to->len == from->len)
+                {
+                    // exact match
+                    std::swap(*to, *from);
+                    break;
+                }
+                else if (to->len > from->len)
+                {
+                    const unsigned diff {to->len - from->len};
+                    std::swap(*to, *from);
+                    from->len = to->len;
+                    disk2.insert(std::next(to), {-1, diff}); // possible iterator invalidation
+                    from = disk2.end();
+                    break;
+                }
+            }
+        }
+        if (actual_id == 0)
+        {
+            break;
+        }
+        --actual_id;
+    }
+
+    uint64_t crc {0};
+    unsigned idx{0};
+    for (auto const& item : disk2)
+    {
+        for (unsigned i {0}; i < item.len; ++i)
+        {
+            if (item.id > 0)
+            {
+                crc += idx * item.id;
+            }
+            ++idx;
+        }
+    }
+
+    fmt::println("2: {}", crc);
+}
+
 int main()
 {
-    Disk disk;
+    Disk1 disk1;
+    Disk2 disk2;
 
     std::string line;
     while(std::getline(std::cin, line))
@@ -60,8 +128,9 @@ int main()
 
             for (int i {0}; i < (c - '0'); ++i)
             {
-                disk.push_back(x);
+                disk1.push_back(x);
             }
+            disk2.push_back({x, (unsigned)(c - '0')});
 
             if ((idx & 1) == 0)
             {
@@ -70,7 +139,8 @@ int main()
         }
     }
 
-    part1(disk);
+    part1(disk1);
+    part2(disk2);
 
     return 0;
 }
