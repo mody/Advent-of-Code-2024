e9165248b36aa1266f0be19c8f21090a8345e7bb
Patrik Modesto <patrik.modesto@gmail.com>
2024-12-16 09:51:12 +0100

    day 11 - part 2

--- src/day11.cc
+++ src/day11.cc
@@ -7,20 +7,23 @@
 #include <boost/algorithm/string/split.hpp>
 
 #include <iostream>
+#include <map>
 #include <vector>
 
-using Data = std::vector<uint64_t>;
+using Data = std::map<uint64_t, uint64_t>;
 
-void part1(Data data)
+uint64_t process(Data const& data, unsigned blinks)
 {
-    for (unsigned blink {0}; blink < 25; ++blink)
+    auto d1 {data};
+
+    for (unsigned blink {0}; blink < blinks; ++blink)
     {
         Data d2;
-        for (auto const v : data)
+        for (auto const& [v, cnt] : d1)
         {
             if (v == 0)
             {
-                d2.push_back(1);
+                d2.insert({1ul, 0}).first->second += cnt;
             }
             else
             {
@@ -30,20 +33,26 @@ void part1(Data data)
                     // even
                     const uint64_t base10div = std::pow(10, v_digits / 2);
                     const uint64_t v1 {v / base10div};
-                    d2.push_back(v1);
-                    d2.push_back(v - (v1 * base10div));
+                    d2.insert({v1, 0}).first->second += cnt;
+                    d2.insert({v - (v1 * base10div), 0}).first->second += cnt;
                 }
                 else
                 {
-                    d2.push_back(v * 2024);
+                    d2.insert({v * 2024ul, 0}).first->second += cnt;
                 }
             }
         }
 
-        std::swap(data, d2);
+        std::swap(d1, d2);
     }
 
-    fmt::println("1: {}", data.size());
+    uint64_t count {0};
+    for (auto const& [_, cnt] : d1)
+    {
+        count += cnt;
+    }
+
+    return count;
 }
 
 
@@ -56,15 +65,16 @@ int main()
                    std::getline(std::cin, line);
                    std::vector<std::string> parts;
                    boost::algorithm::split(parts, line, boost::algorithm::is_any_of(" "), boost::token_compress_on);
-                   result.reserve(parts.size());
                    for (auto const& val : parts)
                    {
-                       result.push_back(std::stoull(val));
+                       result.insert({std::stoull(val), 1});
                    }
                    return result;
                }()};
 
-    part1(data);
+
+    fmt::println("1: {}", process(data, 25));
+    fmt::println("2: {}", process(data, 75));
 
     return 0;
 }
